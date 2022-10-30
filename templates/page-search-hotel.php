98345f6ca955bf84525c025d6fd853affb208b87
khoitq <khoitq.1992@gmail.com>
2022-10-30 23:39:39 +0700

    update css

--- templates/page-search-hotel.php
+++ templates/page-search-hotel.php
@@ -4,92 +4,96 @@
 		<div class="container">
 			<div class="row">
 				<div class="col search-left medium-3">
-					<div class="search-by-hotel-name">
-						<div class="block-title">
-							<h3>Tìm kiếm</h3>
-						</div>
-						<div class="search-group">
-							<input value="" type="text" name="hotel-name" placeholder="Nhập tên khách sạn">
-							<button class="btn-search-by-hotel-name">
-								<i class="icon-search"></i>
-							</button>
-						</div>
-					</div>
-					<div class="price-filter">
-						<div class="block-title">
-							<h3>Ngân sách</h3>
-						</div>
-						<div class="search-group">
-							<div class="search-item">
-							<div id="range-slider">
-								<div id="slider-range"></div>
-								<div class="range-value">
-									<input type="text" id="amount" readonly style="border:0;">
-								</div>
-								<div class="range-submit">
-									<input type="hidden" id="min-filter">
-									<input type="hidden" id="max-filter">
-									<a href="#" class="btn-filter-price btn-normal btn-large text-center mt-2 font_15px">Xem kết quả</a>
-								</div>
-							</div>
-							</div>
-						</div>
-					</div>
-					<div class="search-by-hotel-ranking">
-						<div class="block-title">
-							<h3>Hạng sao</h3>
-						</div>
-						<div class="search-group">
-							<div class="search-item">
-								<input id="input-ranking-1" type="checkbox" name="ranking" value="1">
-								<label for="input-ranking-1"><span class="fake-checkbox"></span><i class="icon-star"></i></label>
-							</div>
-							<div class="search-item">
-								<input id="input-ranking-2" type="checkbox" name="ranking" value="2">
-								<label for="input-ranking-2">
-									<span class="fake-checkbox"></span>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-								</label>
-							</div>
-							<div class="search-item">
-								<input id="input-ranking-3" type="checkbox" name="ranking" value="3">
-								<label for="input-ranking-3">
-									<span class="fake-checkbox"></span>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-								</label>
-							</div>
-							<div class="search-item">
-								<input id="input-ranking-4" type="checkbox" name="ranking" value="4">
-								<label for="input-ranking-4">
-									<span class="fake-checkbox"></span>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-								</label>
-							</div>
-							<div class="search-item">
-								<input id="input-ranking-5" type="checkbox" name="ranking" value="5">
-								<label for="input-ranking-5">
-									<span class="fake-checkbox"></span>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-									<i class="icon-star"></i>
-								</label>
-							</div>
-						</div>
-					</div>
-					<div class="search-by-tx-hotel">
-						<div class="block-title">
-							<h3>Loại hình nhà ở</h3>
-						</div>
-						<div class="search-group">
-							<?php 
+                    <div class="mobile-close">
+                        <i class="fa fa-angle-right"></i>
+                    </div>
+					<div class="filter-content">
+                        <div class="search-by-hotel-name">
+                            <div class="block-title">
+                                <h3>Tìm kiếm</h3>
+                            </div>
+                            <div class="search-group">
+                                <input value="" type="text" name="hotel-name" placeholder="Nhập tên khách sạn">
+                                <button class="btn-search-by-hotel-name">
+                                    <i class="icon-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="price-filter">
+                            <div class="block-title">
+                                <h3>Ngân sách</h3>
+                            </div>
+                            <div class="search-group">
+                                <div class="search-item">
+                                    <div id="range-slider">
+                                        <div id="slider-range"></div>
+                                        <div class="range-value">
+                                            <input type="text" id="amount" readonly style="border:0;">
+                                        </div>
+                                        <div class="range-submit">
+                                            <input type="hidden" id="min-filter">
+                                            <input type="hidden" id="max-filter">
+                                            <a href="#" class="btn-filter-price btn-normal btn-large text-center mt-2 font_15px">Xem kết quả</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="search-by-hotel-ranking">
+                            <div class="block-title">
+                                <h3>Hạng sao</h3>
+                            </div>
+                            <div class="search-group">
+                                <div class="search-item">
+                                    <input id="input-ranking-1" type="checkbox" name="ranking" value="1">
+                                    <label for="input-ranking-1"><span class="fake-checkbox"></span><i class="icon-star"></i></label>
+                                </div>
+                                <div class="search-item">
+                                    <input id="input-ranking-2" type="checkbox" name="ranking" value="2">
+                                    <label for="input-ranking-2">
+                                        <span class="fake-checkbox"></span>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                    </label>
+                                </div>
+                                <div class="search-item">
+                                    <input id="input-ranking-3" type="checkbox" name="ranking" value="3">
+                                    <label for="input-ranking-3">
+                                        <span class="fake-checkbox"></span>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                    </label>
+                                </div>
+                                <div class="search-item">
+                                    <input id="input-ranking-4" type="checkbox" name="ranking" value="4">
+                                    <label for="input-ranking-4">
+                                        <span class="fake-checkbox"></span>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                    </label>
+                                </div>
+                                <div class="search-item">
+                                    <input id="input-ranking-5" type="checkbox" name="ranking" value="5">
+                                    <label for="input-ranking-5">
+                                        <span class="fake-checkbox"></span>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                        <i class="icon-star"></i>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="search-by-tx-hotel">
+                            <div class="block-title">
+                                <h3>Loại hình nhà ở</h3>
+                            </div>
+                            <div class="search-group">
+								<?php
 								$terms_noi_o = get_terms(
 									array(
 										'taxonomy' => 'loai-phong',
@@ -98,21 +102,21 @@
 								);
 								if ( $terms_noi_o ) {
 									foreach( $terms_noi_o as $noi_o ) { ?>
-										<div class="search-item">
-											<input id="input-tx-hotel-<?php echo $noi_o->term_id; ?>" type="checkbox" value="<?php echo $noi_o->term_id; ?>" name="tx_hotel"> 
-											<label for="input-tx-hotel-<?php echo $noi_o->term_id; ?>"> <span class="fake-checkbox"></span><?php echo $noi_o->name; ?></label>
-										</div>
+                                        <div class="search-item">
+                                            <input id="input-tx-hotel-<?php echo $noi_o->term_id; ?>" type="checkbox" value="<?php echo $noi_o->term_id; ?>" name="tx_hotel">
+                                            <label for="input-tx-hotel-<?php echo $noi_o->term_id; ?>"> <span class="fake-checkbox"></span><?php echo $noi_o->name; ?></label>
+                                        </div>
 									<?php }
 								}
-							?>
-						</div>
-					</div>
-					<div class="search-by-convenient">
-						<div class="block-title">
-							<h3>Tiện ích</h3>
-						</div>
-						<div class="search-group">
-							<?php 
+								?>
+                            </div>
+                        </div>
+                        <div class="search-by-convenient">
+                            <div class="block-title">
+                                <h3>Tiện ích</h3>
+                            </div>
+                            <div class="search-group">
+								<?php
 								$terms_tien_ich = get_terms(
 									array(
 										'taxonomy' => 'tien-nghi',
@@ -121,15 +125,16 @@
 								);
 								if ( $terms_tien_ich ) {
 									foreach( $terms_tien_ich as $tien_ich ) { ?>
-										<div class="search-item">
-											<input id="input-tx-convenient-<?php echo $tien_ich->term_id; ?>" type="checkbox" value="<?php echo $tien_ich->term_id; ?>" name="tx_convenient"> 
-											<label for="input-tx-convenient-<?php echo $tien_ich->term_id; ?>"> <span class="fake-checkbox"></span><?php echo $tien_ich->name; ?></label>
-										</div>
+                                        <div class="search-item">
+                                            <input id="input-tx-convenient-<?php echo $tien_ich->term_id; ?>" type="checkbox" value="<?php echo $tien_ich->term_id; ?>" name="tx_convenient">
+                                            <label for="input-tx-convenient-<?php echo $tien_ich->term_id; ?>"> <span class="fake-checkbox"></span><?php echo $tien_ich->name; ?></label>
+                                        </div>
 									<?php }
 								}
-							?>
-						</div>
-					</div>
+								?>
+                            </div>
+                        </div>
+                    </div>
 				</div>
 				<div class="col search-right medium-9">
 					<div class="block-container">
@@ -258,6 +263,16 @@
                 + new Intl.NumberFormat('de-DE').format( sliderRange.slider( "values", 1 ) * 1) + ' đ' ) ;
             $('input#min-filter').val(sliderRange.slider( "values", 0 ) * 1);
             $('input#max-filter').val(sliderRange.slider( "values", 1 ) * 1);
+
+            $(".search-left .mobile-close").click(function () {
+                if ($(this).parent().hasClass("hide")) {
+                    $(this).parent().removeClass("hide");
+                    $(this).parent().css("transform", "translateX(0)");
+                } else {
+                    $(this).parent().addClass("hide");
+                    $(this).parent().css("transform", "translateX(-100%)");
+                }
+            })
 		})
 	})(jQuery)
 </script>
